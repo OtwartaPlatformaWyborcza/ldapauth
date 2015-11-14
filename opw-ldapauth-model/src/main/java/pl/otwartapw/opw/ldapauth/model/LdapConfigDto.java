5e59d92c3b6ac917a066062dbf6c7d0b03c68a42
Adam Kowalewski <github@adamkowalewski.com>
2015-11-14 18:33:59 +0100

    [LDAPAUTH-3] POM cleanup

---
+++ opw-ldapauth-model/src/main/java/pl/otwartapw/opw/ldapauth/model/LdapConfigDto.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Adam Kowalewski.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package pl.otwartapw.opw.ldapauth.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Adam Kowalewski <adamkowalewski.com>
+ */
+public class LdapConfigDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String providerUrl;    
+    private String securityAuthentication;
+    private String searchRoot;
+    private String searchFilter;
+
+    public LdapConfigDto() {
+    }
+
+    public LdapConfigDto(String providerUrl, String securityAuthentication, String searchRoot, String searchFilter) {
+        this.providerUrl = providerUrl;
+        this.securityAuthentication = securityAuthentication;
+        this.searchRoot = searchRoot;
+        this.searchFilter = searchFilter;
+    }
+    
+    
+
+}
