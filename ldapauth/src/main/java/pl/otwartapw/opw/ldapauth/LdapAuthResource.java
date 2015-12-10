b30fdbc26b9cf2b03e686c6f1b94175354c0fb31
Adam Kowalewski <github@adamkowalewski.com>
2015-12-10 16:36:58 +0100

    [LDAPAUTH-6] activated REST service

--- ldapauth/src/main/java/pl/otwartapw/opw/ldapauth/LdapAuthResource.java
+++ ldapauth/src/main/java/pl/otwartapw/opw/ldapauth/LdapAuthResource.java
@@ -1,9 +1,32 @@
 package pl.otwartapw.opw.ldapauth;
 
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import pl.otwartapw.ldapauth.api.LdapAuthApi;
+import pl.otwartapw.ldapauth.api.LoginDto;
+import pl.otwartapw.ldapauth.api.LoginLdapDto;
+import pl.otwartapw.ldapauth.api.VersionDto;
+
 /**
  *
  * @author Adam Kowalewski
  */
-public class LdapAuthResource {
-    
+@Path("/")
+public class LdapAuthResource implements LdapAuthApi {
+
+  @Override
+  public Response login(LoginDto dto) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public Response loginLdap(LoginLdapDto dto) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public VersionDto getVersion() {
+    return VersionBuilder.build();
+  }
+
 }
